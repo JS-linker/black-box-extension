e18803635805888f83df9dea18d458961b0281aa
linker <llin@junglescout.com>
2021-08-05 16:24:25 +0800

    [+] update logic

--- main.js
+++ main.js
@@ -12,21 +12,16 @@ const dotenv = require("dotenv");
   }
 })();
 
-const TEXT_USER_EMAIL = process.env.LOGIN_USER_EMAIL;
-const TEXT_USER_PWD = process.env.LOGIN_USER_PWD;
-const TEXT_URL = process.env.TEXT_AMAZON_URL;
-const TEXT_EXECUTABLE_PATH = process.env.PUPPETEER_EXECUTABLE_PATH;
+const TEST_USER_EMAIL = process.env.LOGIN_USER_EMAIL;
+const TEST_USER_PWD = process.env.LOGIN_USER_PWD;
+const TEST_URL = process.env.TEST_AMAZON_URL;
+const TEST_EXECUTABLE_PATH = process.env.PUPPETEER_EXECUTABLE_PATH;
 const TEST_EXT_PATH = path.join(__dirname, process.env.RELATIVE_EXT_PATH);
-
-// const TEST_EXT_PATH = require("path").join(
-//   __dirname,
-//   "/../../work/extension-pro/jsp/build/chrome"
-// );
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: TEXT_EXECUTABLE_PATH,
+    executablePath: TEST_EXECUTABLE_PATH,
     args: [
       `--disable-extensions-except=${TEST_EXT_PATH}`,
       `--load-extension=${TEST_EXT_PATH}`,
@@ -39,37 +34,19 @@ const TEST_EXT_PATH = path.join(__dirname, process.env.RELATIVE_EXT_PATH);
   const extBackgroundPage = await extBackgroundTarget.page();
   await extBackgroundPage.waitForTimeout(1000 * 3);
 
+  // open new tag goto test_url
   const controlPage = await browser.newPage();
-  await controlPage.goto(TEXT_URL);
+  await controlPage.goto(TEST_URL);
   await controlPage.bringToFront();
 
   await controlPage.waitForSelector("#jsLoginPopup input[id=email]");
   // login input email and pwd
   await controlPage.focus("#jsLoginPopup input[id=email]");
-  controlPage.keyboard.type(TEXT_USER_EMAIL);
+  controlPage.keyboard.type(TEST_USER_EMAIL);
   await controlPage.waitForTimeout(1000 * 1);
   await controlPage.focus("#jsLoginPopup input[id=password]");
-  controlPage.keyboard.type(TEXT_USER_PWD);
+  controlPage.keyboard.type(TEST_USER_PWD);
   await controlPage.waitForTimeout(1000 * 1);
-  // change event no work
-  // await controlPage.$eval(
-  //   "#jsLoginPopup input[id=email]",
-  //   (ele, TEXT_USER_EMAIL) => {
-  //     ele.value = TEXT_USER_EMAIL;
-  //     const event = new Event("change");
-  //     ele.dispatchEvent(event);
-  //   },
-  //   TEXT_USER_EMAIL
-  // );
-  // await controlPage.$eval(
-  //   "#jsLoginPopup input[id=password]",
-  //   (ele, TEXT_USER_PWD) => {
-  //     ele.value = TEXT_USER_PWD;
-  //     const event = new Event("change");
-  //     ele.dispatchEvent(event);
-  //   },
-  //   TEXT_USER_PWD
-  // );
   // logIn
   await controlPage.click("#jsLoginPopup button[type=submit]");
   // close modal
@@ -84,5 +61,5 @@ const TEST_EXT_PATH = path.join(__dirname, process.env.RELATIVE_EXT_PATH);
     return !loadMoreBt.disabled;
   });
   console.timeEnd("LoginAfterDispalyComplate");
-  // await browser.close();
+  await browser.close();
 })();
